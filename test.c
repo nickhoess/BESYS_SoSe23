ecd9b0f6d236e1785234fd8ae9c10e5be02fb594
nickhoess <nick@futuresoftware.de>
2023-03-16 20:13:53 +0100

    test changed

--- test.c
+++ test.c
@@ -5,23 +5,5 @@
 
 int main(int argc, char *argv[])
 {
-    int x = 100;
-    int rc = fork();
-
-    if(rc < 0)
-    {
-        fprintf(stderr, "fork failed\n");
-        exit(1);
-    } else if(rc == 0)
-    {
-        printf("Children X: %d\n", x);
-        x = 1;
-        printf("Children X_new: %d\n", x);
-    } else {
-        wait(NULL);
-        printf("Parent X: %d\n", x);
-        x = 2;
-        printf("Parent X_new: %d\n", x);
-    }
-    return 0;
+    printf('hallo im working');
 }
