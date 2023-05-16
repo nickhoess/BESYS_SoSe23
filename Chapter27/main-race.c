319931dea46e2f1090689b8751d58b2b8eb22faf
nickhoess <nick@futuresoftware.de>
2023-05-16 11:57:58 +0200

    fixed lock in main-race

--- Chapter27/main-race.c
+++ Chapter27/main-race.c
@@ -8,18 +8,18 @@ pthread_mutex_t l2 = PTHREAD_MUTEX_INITIALIZER;
 int balance = 0;
 
 void* worker(void* arg) {
-    /*Pthread_mutex_lock(&l1);
+    Pthread_mutex_lock(&l1);
     balance++; // unprotected access
-    Pthread_mutex_unlock(&l1)*/
+    Pthread_mutex_unlock(&l1)
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
-    Pthread_mutex_lock(&l2);
+    Pthread_mutex_lock(&l1);
     balance++; // unprotected access
-    Pthread_mutex_lock(&l2);
+    Pthread_mutex_lock(&l1);
     Pthread_join(p, NULL);
     return 0;
     return 0;
